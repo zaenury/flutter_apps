121c4dfc947dca0869af5011744f1e62834b597e
zaenury <adhiimzaenury@gmail.com>
2020-12-25 17:07:59 +0800

    count button stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyContainer());
+  runApp(new CountButton());
+}
+
+class CountButton extends StatefulWidget {
+  @override
+  _CountButtonState createState() => _CountButtonState();
+}
+
+class _CountButtonState extends State<CountButton> {
+  int number = 0;
+  void tekanTombol() {
+    setState(() {
+      number += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Statefull Widget Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              RaisedButton(
+                child: Text("Tekan Saya"),
+                onPressed: tekanTombol,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyContainer extends StatelessWidget {
