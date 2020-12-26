3464cb3377911037da3a30957f73c87336cda818
zaenury <adhiimzaenury@gmail.com>
2020-12-26 18:38:26 +0800

    3 stack & align button position

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,173 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new FlexibleWidgetApp());
+  runApp(new MyStack());
+}
+
+class MyStack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Stack & Align"),
+        ),
+        body: Stack(
+          // layer 1
+          children: <Widget>[
+            Column(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.white),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.black12),
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.black12),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            // layer 2
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: [
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Ini Adalah Text View",
+                      style: TextStyle(fontSize: 30),
+                    )
+                  ],
+                )
+              ],
+            ),
+            // layer 3
+            Align(
+              alignment: Alignment(0, 0.8),
+              child: RaisedButton(
+                child: Text("My Button"),
+                color: Colors.amber,
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class FlexibleWidgetApp extends StatelessWidget {
