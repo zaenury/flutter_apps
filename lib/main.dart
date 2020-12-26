48c9aa77573a1535ddf311bcfb68b23962b69ad7
zaenury <adhiimzaenury@gmail.com>
2020-12-26 11:36:00 +0800

    text style example & custom font

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new AnonymousMethod());
+  runApp(new TextStyleExample());
+}
+
+class TextStyleExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Text Style"),
+        ),
+        body: Center(
+          child: Text("INI ADALAH TEXT",
+              style: TextStyle(
+                  fontFamily: "crashlandingBB",
+                  fontSize: 40,
+                  fontStyle: FontStyle.italic,
+                  decoration: TextDecoration.overline,
+                  decorationColor: Colors.red,
+                  decorationThickness: 5,
+                  decorationStyle: TextDecorationStyle.wavy)),
+        ),
+      ),
+    );
+  }
 }
 
 class AnonymousMethod extends StatefulWidget {
