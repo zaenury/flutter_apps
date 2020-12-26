1b715d7c144ee7c4d9e74e7d0d9309ee65cc6e73
zaenury <adhiimzaenury@gmail.com>
2020-12-26 16:05:30 +0800

    flexible widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,66 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new AnimationOnContainer());
+  runApp(new FlexibleWidgetApp());
+}
+
+class FlexibleWidgetApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flexible Widget"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.amber,
+                      margin: EdgeInsets.all(5),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.deepOrangeAccent,
+                      margin: EdgeInsets.all(5),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.greenAccent,
+                      margin: EdgeInsets.all(5),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                color: Colors.blueAccent,
+                margin: EdgeInsets.all(5),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.brown,
+                margin: EdgeInsets.all(5),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class AnimationOnContainer extends StatefulWidget {
