38586a4618685082a9c69a30717eb26b1f110d75
zaenury <adhiimzaenury@gmail.com>
2020-12-26 12:53:30 +0800

    animated container with gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new TextStyleExample());
+  runApp(new AnimationOnContainer());
+}
+
+class AnimationOnContainer extends StatefulWidget {
+  @override
+  _AnimationOnContainerState createState() => _AnimationOnContainerState();
+}
+
+class _AnimationOnContainerState extends State<AnimationOnContainer> {
+  Random random = Random(); // auto import 'dart:math';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Animation Widget"),
+        ),
+        body: Center(
+          // Pakai GestureDetector karena AnimatedContainer tidak punya event didalamnya.
+          child: GestureDetector(
+            onTap: () {
+              setState(() {}); // hanya merefresh halaman
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256),
+                  random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(201),
+              height: 50.0 + random.nextInt(201),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class TextStyleExample extends StatelessWidget {
