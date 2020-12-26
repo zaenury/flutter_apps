4ab53f2fa28427cac544c66d78b757dcbda1f2a3
zaenury <adhiimzaenury@gmail.com>
2020-12-26 10:49:08 +0800

    anonymous method

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,52 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new ListExample());
+  runApp(new AnonymousMethod());
+}
+
+class AnonymousMethod extends StatefulWidget {
+  @override
+  _AnonymousMethodState createState() => _AnonymousMethodState();
+}
+
+class _AnonymousMethodState extends State<AnonymousMethod> {
+  String message = "Ini adalah text";
+
+  // Kalau cuma sekali pakai tidak butuh deklrasai method dengan nama.
+  // Langsung pakai ditempat yang diinginkan
+  // void clickedButton() {
+  //   setState(() {
+  //     message = "Tombol ini sudah ditekan";
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(message),
+              RaisedButton(
+                child: Text("tekan tombol"),
+                //anonymouse method (method tanpa nama)
+                onPressed: () {
+                  setState(() {
+                    message = "Tombol ini sudah ditekan";
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class ListExample extends StatefulWidget {
