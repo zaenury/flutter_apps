96dcea8763cb85ad5cb7f6611bb05ab8337ab5ec
zaenury <adhiimzaenury@gmail.com>
2020-12-28 13:59:04 +0800

    asset image & network image

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,38 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyStack());
+  runApp(new MyImageWidget());
+}
+
+class MyImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Image Widget"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.black,
+            padding: EdgeInsets.all(3),
+            height: 120,
+            width: 120,
+            child: Image(
+              // Image from network
+              // image: NetworkImage(
+              //     "https://avatars2.githubusercontent.com/u/42806183?s=460&u=224a1be61931272bc98a98372d89f2715cd5091e&v=4"),
+
+              // Image from assets
+              image: AssetImage("images/flutter-image.png"),
+              fit: BoxFit.contain,
+              //repeat: ImageRepeat.repeat,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyStack extends StatelessWidget {
